55de75c0892bece3c8e0700bf51d7e80f1d673c4
Daniel <peer77@gmail.com>
2019-11-04 08:53:50 +0200

    add font awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +41,7 @@ import { EnumKeyPipe } from './pipes/enum-keys.pipe';
     NgbModule,
     FormsModule,
     HttpClientModule,
+    AngularFontAwesomeModule,
   ],
   entryComponents: [
     ModalContentComponent,
